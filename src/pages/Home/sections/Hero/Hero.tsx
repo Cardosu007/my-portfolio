a8bf10b7b6fc6c85eacf73f3b3d3811c8bfc7595
W Cardoso <cardosu7@gmail.com>
2024-06-02 15:41:25 -0300

    create button, navbar

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,19 +1,25 @@
-import { Button, Container, Grid, Typography, styled } from "@mui/material"
-import Avatar from "../../../../assets/images/avatar.png"
+import { Box, Container, Grid, Typography, styled } from "@mui/material"
+import Avatar from "../../../../assets/images/avatar1.jpg"
 import DownloadIcon from '@mui/icons-material/Download';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import StyledButton from "../../../../components/StyledButton/StyledButton";
+import theme from "../../../../theme";
+import { AnimatedBackground } from "../../../../components/AnimatedBackground/AnimatedBackground";
 
 const Hero = () => {
 
 
-    const StyledHero = styled("div")(() => ({
-        backgroundColor: "black",
-        height: "100vh"
+    const StyledHero = styled("div")(({ theme }) => ({
+        backgroundColor: theme.palette.primary.main,
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
     }))
 
-    const StyledImg = styled("img")(() => ({
-        width: "100%",
-        borderRadius: "50%"
+    const StyledImg = styled("img")(({ theme }) => ({
+        width: "80%",
+        borderRadius: "50%",
+        border: `1px solid ${theme.palette.primary.contrastText}`, 
     }))
 
 
@@ -24,22 +30,31 @@ const Hero = () => {
 
                 <Container maxWidth="lg">
                     <Grid container spacing={2}>
-                        <Grid item xs={12} md={4}>
-                            <StyledImg src={Avatar} />
+                        <Grid item xs={12} md={5}>
+                            <Box position="relative">
+                                <Box position="absolute" width={"150%"} top={-100} right={0}>
+                                    <AnimatedBackground></AnimatedBackground>
+                                </Box>
+                                <Box position="relative" textAlign="center">
+                                    <StyledImg src={Avatar} />
+                                </Box>
+                            </Box>  
                         </Grid>
-                        <Grid item xs={12} md={8}>
-                            <Typography color="primary" variant="h1" textAlign="center">Walter Cardoso</Typography>
-                            <Typography color="primary" variant="h2" textAlign="center">I am a Front-End Developer</Typography>
-                            <Grid container display="flex" justifyContent="center">
+                        <Grid item xs={12} md={7}>
+                            <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Walter Cardoso</Typography>
+                            <Typography color="primary.contrastText" variant="h2" textAlign="center">I am a Front-End Developer</Typography>
+                            <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
                                 <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                                <Button>
-                            <DownloadIcon/>
-                            Download CV</Button>
+                            <StyledButton>
+                                <DownloadIcon/>
+                            <Typography>Download CV</Typography>
+                            </StyledButton>
                                 </Grid>
                                 <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                                <Button>
+                                <StyledButton>
                             <MailOutlineIcon/>
-                                Contact me</Button>
+                                <Typography>Contact me</Typography>
+                                </StyledButton>
                                 </Grid>
                             </Grid>
                         </Grid>
